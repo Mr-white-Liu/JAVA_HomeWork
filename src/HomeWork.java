9ed68053cfec23fef7564493b7686b730da3f579
Mr-white-Liu <2335213424@qq.com>
2020-02-23 14:53:20 +0800

    不使用Math类

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class HomeWork {
     public static void main(String[] args) {
+        int powerSum = 0;
         int sum = 0;
         System.out.println("请输入一个数字");
         System.out.println("例如：输入 10 ，程序将会找到 10 以内，存在使得连续数字立方和等于另一数字立方和的等式");
@@ -15,27 +16,29 @@ public class HomeWork {
         for (; i >= 2; i--) {
             for (int j = 1; j < i; j++) {
                 for (int k = j; k <= i; k++) {
-                    sum += list[k];
+                    powerSum += list[k];
+                    sum += k;
                 }
-                if (Math.cbrt(sum) == (int) Math.cbrt(sum)) {
-                    int flg=0;
-                    for (int k = j; k < i; k++) {
-                        if(flg==15){
-                            System.out.println();
-                            flg=0;
+                for (int tag = i; tag < sum; tag++) {
+                    if (powerSum == tag * tag * tag) {
+                        int flg = 0;
+                        for (int k = j; k < i; k++) {
+                            if (flg == 15) {
+                                System.out.println();
+                                flg = 0;
+                            }
+                            System.out.print(k + "^3 + ");
+                            flg++;
                         }
-                        System.out.print(k + "^3 + ");
-                        flg++;
+                        System.out.print(i + "^3 = ");
+                        System.out.println(tag + "^3");
+                        System.out.println();
+                        break;
                     }
-                    System.out.print(i + "^3 = ");
-                    System.out.println((int)Math.cbrt(sum) + "^3");
-                    sum = 0;
-                    System.out.println();
                 }
-                else
-                    sum = 0;
+                powerSum = 0;
+                sum = 0;
             }
-
         }
     }
 }
